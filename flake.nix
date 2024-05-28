d1f97b0a8933a6de38c5662975c29c9d45d527fb
Omer Hamerman <omerxx@gmail.com>
2024-05-28 08:54:44 +0100

    fix linux

--- flake.nix
+++ flake.nix
@@ -1,28 +1,39 @@
 {
-	inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-23.05-darwin";
-	outputs = { self, nixpkgs }: {
-		defaultPackage = {
-			aarch64-darwin = buildPackage "aarch64-darwin";
-			x86_64-linux = buildPackage "x86_64-linux";
-		}
-		buildPackage = system: 
-			let 
-			pkgs = import nixpkgs {
-				inherit system;
-			};
-			in 
-			pkgs.stdenv.mkDerivation {
-				name = "hello";
-				src = ./.;
-				buildInputs = [ pkgs.go ];
-				buildPhase = ''
-					export GOCACHE=$(mktemp -d)
-					cd hello
-					go build -o hello
-					'';
-				installPhase = ''
-					mkdir -p $out/bin
-					install -t $out/bin hello
-					'';
-			};
-	}
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-23.05-darwin";
+
+  outputs = { self, nixpkgs }: {
+    defaultPackage = {
+      aarch64-darwin = let
+        pkgs = import nixpkgs { system = "aarch64-darwin"; };
+      in pkgs.stdenv.mkDerivation {
+        name = "hello";
+        src = self;
+        buildInputs = [ pkgs.go ];
+        buildPhase = ''
+          export GOCACHE=$(mktemp -d)
+          go build -o hello main.go
+        '';
+        installPhase = ''
+          mkdir -p $out/bin
+          install -t $out/bin hello
+        '';
+      };
+
+      x86_64-linux = let
+        pkgs = import nixpkgs { system = "x86_64-linux"; };
+      in pkgs.stdenv.mkDerivation {
+        name = "hello";
+        src = self;
+        buildInputs = [ pkgs.go ];
+        buildPhase = ''
+          export GOCACHE=$(mktemp -d)
+          go build -o hello main.go
+        '';
+        installPhase = ''
+          mkdir -p $out/bin
+          install -t $out/bin hello
+        '';
+      };
+    };
+  };
+}
